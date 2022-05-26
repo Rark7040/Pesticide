97efbadeb735bc62109958bb5722087161a7cd2f
Rark7040 <crimera.hello@gmail.com>
2022-05-26 13:12:49 +0900

    load config

---
+++ src/rarkhopper/pesticide/ConfigLoader.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace rarkhopper\pesticide;
+
+use pocketmine\utils\Config;
+
+class ConfigLoader{
+
+	public function __construct(Config $conf){}
+
+	public function load():void{}
+}
