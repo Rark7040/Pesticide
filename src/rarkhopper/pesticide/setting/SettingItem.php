61851c14bf0616b96ccfd0755631caef866276b6
Rark7040 <crimera.hello@gmail.com>
2022-06-07 12:59:39 +0900

    SETTING ITEMS

--- src/rarkhopper/pesticide/setting/SettingItem.php
+++ src/rarkhopper/pesticide/setting/SettingItem.php
@@ -4,13 +4,38 @@ declare(strict_types=1);
 namespace rarkhopper\pesticide\setting;
 
 abstract class SettingItem{
-	protected string $name;
-
-	abstract public function getName():string;
-	/** @return array<static> */
-	abstract public function getChildren():array;
-
-	public function __construct(string $name){
-		$this->name = $name;
+	final private function __construct()
+	{
+		//NOOP
 	}
+
+	const PENALTY = 'penalty';
+	const ANTI_SPEED = 'anti_speed';
+	const ANTI_SPEED__ENABLE = 'enable';
+	const ANTI_SPEED__LIMIT_DISTANCE = 'limit_distance';
+	const ANTI_REACH = 'anti_reach';
+	const ANTI_REACH__ENABLE = 'enable';
+	const ANTI_REACH__LIMIT_DISTANCE = 'limit_distance';
+	const ANTI_AUTO_CLICKER = 'anti_auto_clicker';
+	const ANTI_AUTO_CLICKER__ENABLE = 'enable';
+	const ANTI_AUTO_CLICKER__LIMIT_CPS = 'limit_cps';
+	const ANTI_AUTO_CLICKER__LEFT_CLICK = 'left_click';
+	const ANTI_AUTO_CLICKER__RIGHT_CLICK = 'right_click';
+	const ANTI_NUKER = 'anti_nuker';
+	const ANTI_NUKER__ENABLE = 'enable';
+	const ANTI_NUKER__LIMIT_BREAK_CNT = 'limit_break_cnt';
+	const ANTI_SPAM = 'anti_spam';
+	const ANTI_SPAM__ENABLE = 'enable';
+	const ANTI_SPAM__LIMIT_SPEED = 'limit_speed';
+	const ANTI_SPAM__BUFFER_MODE = 'buffer_mode';
+	const ANTI_SPAM__BUFFER_MODE__ENABLE = 'enable';
+	const ANTI_FLY = 'anti_fly';
+	const ANTI_FLY__ENABLE = 'enable';
+	const ANTI_FLY__OVERLOOK_CNT = 'overlook_cnt';
+	const ANTI_NOCLIP = 'anti_noclip';
+	const ANTI_NOClIP__ENABLE = 'enable';
+	const ANTI_ANTI_IMMOBILE = 'anti_anti-immobile';
+	const ANTI_ANTI_IMMOBILE__ENABLE = 'enable';
+	const ANTI_INSTABREAK = 'anti_instabreak';
+	const ANTI_INSTABREAK__ENABLE = 'enable';
 }
