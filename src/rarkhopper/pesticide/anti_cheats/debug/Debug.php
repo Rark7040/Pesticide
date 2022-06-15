f264211b2d3875de26ec0b2e55c6fe95d8d75a71
Rark7040 <crimera.hello@gmail.com>
2022-06-16 01:08:40 +0900

    debug listener

--- src/rarkhopper/pesticide/anti_cheats/debug/Debug.php
+++ src/rarkhopper/pesticide/anti_cheats/debug/Debug.php
@@ -6,7 +6,15 @@ namespace rarkhopper\pesticide\anti_cheats\debug;
 use rarkhopper\pesticide\anti_cheats\AntiCheat;
 
 class Debug extends AntiCheat{
+
+	public function init():void{
+		parent::init();
+		//register task and more...
+	}
+
 	public function getListeners():array{
-		return [];
+		return [
+			new DebugListener
+		];
 	}
 }
---
+++ src/rarkhopper/pesticide/anti_cheats/debug/DebugListener.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace rarkhopper\pesticide\anti_cheats\debug;
+
+use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketReceiveEvent;
+
+class DebugListener implements Listener{
+	public function onReceivePackets(DataPacketReceiveEvent $ev):void{
+
+	}
+}
